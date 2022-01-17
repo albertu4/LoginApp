cbf9c1ddddace74ba450d321a5eb2d99ebd903ac
Miwka <97275239+albertu4@users.noreply.github.com>
2022-01-17 17:14:59 +0900

    logic of VC's in process

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+ 
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    var welcome: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -16,7 +22,33 @@ class LoginViewController: UIViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController
+        else { return }
+        welcomeVC.welcome = userNameTF.text
+    }
 
-
+    @IBAction func loginButtonPressed() {
+    }
+    
+    @IBAction func forgotNameButtonPressed() {
+        showAlert(title: "Oops!", and: "Your name is User")
+    }
+    
+    @IBAction func ForgotPasswordButtonPressed() {
+        showAlert(title: "Oops!", and: "Your password is 12345678")
+    }
+    
+    private func showAlert (title: String, and message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
 
--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -9,19 +9,17 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    @IBOutlet var welcomeUserText: UILabel!
+    
+    var welcome: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let text = welcome else { return }
+        welcomeUserText.text = "Welcome, \(text)"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logOutButtonPressed() {
     }
-    */
-
+    
 }
